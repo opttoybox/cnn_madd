47159504179f7c58b04d22a087e3c8c5b33ae2be
opttoybox <nrfm92@gmail.com>
2019-05-02 17:14:50 +0900

    change specification

--- cnn_madd/cnn_madd.py
+++ cnn_madd/cnn_madd.py
@@ -4,51 +4,53 @@ import math
 
 class Network:
 
-    def __init__(self, shape, name=''):
-        self.ret_dict = {'n_mul' : 0, 
-                        'n_add' : 0, 
-                        'input' : shape, 
-                        'shape' : shape, 
-                        'name'  : name,
-                        'child' : [],
-                        }
+    def __init__(self, input, name=''):
+        self.layer = {'n_mul'  : 0, 
+                      'n_add'  : 0, 
+                      'input'  : input, 
+                      'output' : input, 
+                      'name'   : name,
+                      'child'  : [],
+                     }
 
     def nest(self, dict):
-        self.ret_dict['child'].append(dict)
-        self.ret_dict['n_mul'] += dict['n_mul']
-        self.ret_dict['n_add'] += dict['n_add']
-        self.ret_dict['shape']  = dict['shape']
+        self.layer['child'].append(dict)
+        self.layer['n_mul'] += dict['n_mul']
+        self.layer['n_add'] += dict['n_add']
+        self.layer['input']  = dict['input']
+        self.layer['output'] = dict['output']
 
         return dict
         
     def del_layers(self, child_idx_begin, child_idx_end=None):
 
-        n_child = len(self.ret_dict['child'])
+        n_child = len(self.layer['child'])
         child_idx_begin = child_idx_begin if 0 < child_idx_begin else n_child + child_idx_begin
         child_idx_end   = child_idx_end if child_idx_end is not None else n_child-1
         child_idx_end   = child_idx_end if 0 < child_idx_end else n_child + child_idx_end
 
         for idx in range(child_idx_begin, child_idx_end+1):
-            child = self.ret_dict['child'][idx]
-            self.ret_dict['n_mul'] -= child['n_mul']
-            self.ret_dict['n_add'] -= child['n_add']
+            child = self.layer['child'][idx]
+            self.layer['n_mul'] -= child['n_mul']
+            self.layer['n_add'] -= child['n_add']
 
-        del self.ret_dict['child'][child_idx_begin:child_idx_end+1]
+        del self.layer['child'][child_idx_begin:child_idx_end+1]
 
-        if 0 < len(self.ret_dict['child']):
-            last_child = self.ret_dict['child'][-1]
-            self.ret_dict['shape'] = last_child['shape']
+        if 0 < len(self.layer['child']):
+            last_child = self.layer['child'][-1]
+            self.layer['output'] = last_child['output']
 
-        return self.ret_dict
+        return self.layer.copy()
 
     
     def return_dict(self):
-        return self.ret_dict
+        return self.layer.copy()
     
 
-def conv(shape, c_out, k_size=[3, 3], stride=[1, 1], padding=True):
-
-    h_in, w_in, c_in = shape
+def conv(input, c_out=None, k_size=[3, 3], stride=[1, 1], padding=True):
+    input = input['output'] if isinstance(input, dict) else input
+    h_in, w_in, c_in = input
+    c_out = c_out if c_out is not None else c_in
     h_k, w_k = k_size
     s_x, s_y = stride
     if padding:
@@ -57,7 +59,6 @@ def conv(shape, c_out, k_size=[3, 3], stride=[1, 1], padding=True):
     else:
         h_out = int(math.ceil((h_in-(h_k-1)+(1-h_k%2))/s_y))
         w_out = int(math.ceil((w_in-(w_k-1)+(1-w_k%2))/s_x))
-
 
     n_mul = h_k * w_k * h_out * w_out * c_in * c_out
     n_add = ((h_k * w_k - 1) * h_out * w_out + (c_in - 1)) * c_out \
@@ -65,22 +66,25 @@ def conv(shape, c_out, k_size=[3, 3], stride=[1, 1], padding=True):
 
     return {'n_mul'  : n_mul, 
             'n_add'  : n_add, 
-            'shape'  : [h_out, w_out, c_out],
+            'input'  : input,
+            'output' : [h_out, w_out, c_out],
             'name'   : 'convolution',
             }
 
 
-def bn(shape):
-    return {'n_mul'  : shape[0]*shape[1]*shape[2], 
-            'n_add'  : shape[0]*shape[1]*shape[2], 
-            'shape'  : shape,
+def bn(input):
+    input = input['output'] if isinstance(input, dict) else input
+    return {'n_mul'  : input[0]*input[1]*input[2], 
+            'n_add'  : input[0]*input[1]*input[2], 
+            'input'  : input,
+            'output' : input,
             'name'   : 'batch normalization',
             }
 
 
-def pool(shape, k_size=[2, 2], stride=[2, 2], padding=False):
-    
-    h_in, w_in, c_in = shape
+def pool(input, k_size=[2, 2], stride=[2, 2], padding=False):
+    input = input['output'] if isinstance(input, dict) else input
+    h_in, w_in, c_in = input
     h_k, w_k = k_size
     s_x, s_y = stride
 
@@ -90,149 +94,218 @@ def pool(shape, k_size=[2, 2], stride=[2, 2], padding=False):
     else:
         h_out = int(math.ceil((h_in-(h_k-1)+(1-h_k%2))/s_y))
         w_out = int(math.ceil((w_in-(w_k-1)+(1-w_k%2))/s_x))
+
     return {'n_mul'  : 0, 
-            'n_add'  : 0, 
-            'shape'  : [h_out, w_out, c_in],
+            'n_add'  : 0,
+            'input'  : [h_in, w_in, c_in],
+            'output' : [h_out, w_out, c_in],
             'name'   : 'pooling',
             }
 
 
-def add(shape):
+def add(input):
+    input = input['output'] if isinstance(input, dict) else input
     return {'n_mul'  : 0, 
-            'n_add'  : shape[0]*shape[1]*shape[2], 
-            'shape'  : shape,
+            'n_add'  : input[0]*input[1]*input[2], 
+            'input'  : input,
+            'output' : input,
             'name'   : 'addition',
             }
 
 
-def flat(shape):
+def concat(dict_list, output=None):
+    c = sum([dict['output'][2] for dict in dict_list])
+    h, w, _c = output if output is not None else max(dict_list, key=lambda x: x['output'][0]*x['output'][1])['output']
+    return {'n_mul'  : 0,
+            'n_add'  : 0,
+            'input'  : [h, w, c],
+            'output' : [h, w, c],
+            'name'   : 'concatenate'
+            }
+
+
+def flat(input):
+    input = input['output'] if isinstance(input, dict) else input
     return {'n_mul'  : 0, 
             'n_add'  : 0, 
-            'shape'  : [1, 1, shape[0]*shape[1]*shape[2]],
+            'input'  : input, 
+            'output' : [1, 1, input[0]*input[1]*input[2]],
             'name'   : 'flatten',
             }
 
 
-def gap(shape):
-    return {'n_mul'  : shape[2], 
-            'n_add'  : (shape[0]*shape[1]-1)*shape[2], 
-            'shape'  : [1, 1, shape[2]],
+def gap(input):
+    input = input['output'] if isinstance(input, dict) else input
+    return {'n_mul'  : input[2], 
+            'n_add'  : (input[0]*input[1]-1)*input[2], 
+            'input'  : input, 
+            'output' : [1, 1, input[2]],
             'name'   : 'global average pooling',
             }
 
 
-def fc(c_in, c_out):
-    return {'n_mul' : c_in * c_out, 
-            'n_add' : (c_in - 1) * c_out + c_out, 
-            'shape' : [1, 1, c_out],
-            'name'  : 'full connection',
+def fc(input, c_out):
+    c_in = input['output'][2] if isinstance(input, dict) else input
+    return {'n_mul'  : c_in * c_out, 
+            'n_add'  : (c_in - 1) * c_out + c_out, 
+            'input'  : [1, 1, c_in], 
+            'output' : [1, 1, c_out],
+            'name'   : 'full connection',
             }
 
 
-def d_conv(shape, k_size, stride=[1, 1]):
+def d_conv(input, k_size, stride=[1, 1]):
+    input = input['output'] if isinstance(input, dict) else input    
+    *size, c_out = input
+    layer = conv([*size, 1], c_out, k_size, stride)
+    layer['name'] = 'depthwise convolution'
+
+    return layer
+
+
+def p_conv(input, c_out=None, stride=[1, 1]):
+    input = input['output'] if isinstance(input, dict) else input    
+    layer = conv(input, c_out, k_size=[1, 1], stride=stride)
+    layer['name'] = 'pointwise convolution'
+
+    return layer
+
+
+def sepconv(input, c_out, k_size=[3, 3], stride=[1, 1]):
+    input = input['output'] if isinstance(input, dict) else input    
+    layer = Network(input, 'separable convolution')
+    tmp_dict = layer.nest(d_conv(output, k_size, stride))
+    tmp_dict = layer.nest(bn(tmp_dict['output']))
+    tmp_dict = layer.nest(p_conv(tmp_dict['output'], c_out, stride=[1, 1]))
+    tmp_dict = layer.nest(bn(tmp_dict['output']))
+
+    return layer.return_dict()
+
+
+def print_madd(dict, print_all=True, indent='  ', cnt=0):
     
-    *size, c_out = shape
-    ret_dict = conv([*size, 1], c_out, k_size, stride)
-    ret_dict['name'] = 'depthwise convolution'
-
-    return ret_dict
-
-
-def p_conv(shape, c_out, stride=[1, 1]):
-    
-    ret_dict = conv(shape, c_out, k_size=[1, 1], stride=stride)
-    ret_dict['name'] = 'pointwise convolution'
-
-    return ret_dict
+    if print_all or cnt==0:
+        print(indent*cnt + '- ' + dict['name'])
+        print(indent*(cnt+1) + '  n_mul : {:,}'.format(dict['n_mul']))
+        print(indent*(cnt+1) + '  n_add : {:,}'.format(dict['n_add']))
+        print(indent*(cnt+1) + '  input : ' + str(dict['input']))
+        print(indent*(cnt+1) + '  output: ' + str(dict['output']))
+        print('')  
+    else:
+        print(indent*cnt + '- ' + dict['name'])
 
 
-def sepconv(shape, c_out, k_size=[3, 3], stride=[1, 1]):
-    
-    ret_dict = Network('separable convolution')
-    tmp_dict = ret_dict.nest(d_conv(shape, k_size, stride))
-    tmp_dict = ret_dict.nest(bn(tmp_dict['shape']))
-    tmp_dict = ret_dict.nest(p_conv(tmp_dict['shape'], c_out, stride=[1, 1]))
-    tmp_dict = ret_dict.nest(bn(tmp_dict['shape']))
-
-    return ret_dict.return_dict()
-
-
-def print_madd(dict, print_tree=True, indent='  ', cnt=0):
-    
-    print(indent*cnt + '- ' + dict['name'])
-    print(indent*(cnt+1) + '  n_mul: {:,}'.format(dict['n_mul']))
-    print(indent*(cnt+1) + '  n_add: {:,}'.format(dict['n_add']))
-    print(indent*(cnt+1) + '  shape: ' + str(dict['shape']))
-    print('')  
-
-    if ('child' in dict.keys()) and print_tree:
+    if ('child' in dict.keys()):
         if 0 < len(dict['child']) : cnt += 1
-        for child in dict['child']: print_madd(child, print_tree=print_tree, indent=indent, cnt=cnt)        
+        for child in dict['child']: print_madd(child, print_all=print_all, indent=indent, cnt=cnt)        
 
 
 class AlexNet(Network):
 
-    def __init__(self, shape=[227, 227, 3], name='AlexNet'):
+    def __init__(self, input=[227, 227, 3], name='AlexNet'):
 
-        super().__init__(shape, name=name)
-        temp = self.ret_dict
-        temp = self.nest(conv(temp['shape'], 96, [11, 11], stride=[4, 4], padding=False))
-        temp = self.nest(pool(temp['shape'], k_size=[3, 3], stride=[2, 2]))
+        super().__init__(input, name=name)
+        temp = self.layer
+        temp = self.nest(conv(temp, 96, [11, 11], stride=[4, 4], padding=False))
+        temp = self.nest(pool(temp, k_size=[3, 3], stride=[2, 2]))
 
-        temp = self.nest(conv(temp['shape'], 256, [5, 5]))
-        temp = self.nest(pool(temp['shape'], k_size=[3, 3], stride=[2, 2]))
+        temp = self.nest(conv(temp, 256, [5, 5]))
+        temp = self.nest(pool(temp, k_size=[3, 3], stride=[2, 2]))
 
-        temp = self.nest(conv(temp['shape'], 384))
-        temp = self.nest(conv(temp['shape'], 384))
-        temp = self.nest(conv(temp['shape'], 256))
-        temp = self.nest(pool(temp['shape'], k_size=[3, 3], stride=[2, 2]))
-        temp = self.nest(flat(temp['shape']))
-        temp = self.nest(fc(temp['shape'][2], 4096))
-        temp = self.nest(fc(temp['shape'][2], temp['shape'][2]))
-        temp = self.nest(fc(temp['shape'][2], 1000))
+        temp = self.nest(conv(temp, 384))
+        temp = self.nest(conv(temp, 384))
+        temp = self.nest(conv(temp, 256))
+        temp = self.nest(pool(temp, k_size=[3, 3], stride=[2, 2]))
+        temp = self.nest(flat(temp))
+        temp = self.nest(fc(temp, 4096))
+        temp = self.nest(fc(temp, temp['output'][2]))
+        temp = self.nest(fc(temp, 1000))
 
 
 class VGG16(Network):
 
-    def __init__(self, shape=[224, 224, 3], name='VGG16'):
+    def __init__(self, input=[224, 224, 3], name='VGG16'):
 
-        super().__init__(shape, name=name)
-        temp = self.ret_dict
-        temp = self.nest(conv(temp['shape'], 64))
-        temp = self.nest(conv(temp['shape'], temp['shape'][2]))
-        temp = self.nest(pool(temp['shape']))
+        super().__init__(input, name=name)
+        temp = self.layer
+        temp = self.nest(conv(temp, 64))
+        temp = self.nest(conv(temp))
+        temp = self.nest(pool(temp))
 
-        temp = self.nest(conv(temp['shape'], temp['shape'][2]*2))
-        temp = self.nest(conv(temp['shape'], temp['shape'][2]))
-        temp = self.nest(pool(temp['shape']))
+        temp = self.nest(conv(temp, temp['output'][2]*2))
+        temp = self.nest(conv(temp))
+        temp = self.nest(pool(temp))
 
-        temp = self.nest(conv(temp['shape'], temp['shape'][2]*2))
-        temp = self.nest(conv(temp['shape'], temp['shape'][2]))
-        temp = self.nest(conv(temp['shape'], temp['shape'][2]))
-        temp = self.nest(pool(temp['shape']))
+        temp = self.nest(conv(temp, temp['output'][2]*2))
+        temp = self.nest(conv(temp))
+        temp = self.nest(conv(temp))
+        temp = self.nest(pool(temp))
 
-        temp = self.nest(conv(temp['shape'], temp['shape'][2]*2))
-        temp = self.nest(conv(temp['shape'], temp['shape'][2]))
-        temp = self.nest(conv(temp['shape'], temp['shape'][2]))
-        temp = self.nest(pool(temp['shape']))
+        temp = self.nest(conv(temp, temp['output'][2]*2))
+        temp = self.nest(conv(temp))
+        temp = self.nest(conv(temp))
+        temp = self.nest(pool(temp))
 
-        temp = self.nest(conv(temp['shape'], temp['shape'][2]))
-        temp = self.nest(conv(temp['shape'], temp['shape'][2]))
-        temp = self.nest(conv(temp['shape'], temp['shape'][2]))
-        temp = self.nest(pool(temp['shape']))
+        temp = self.nest(conv(temp))
+        temp = self.nest(conv(temp))
+        temp = self.nest(conv(temp))
+        temp = self.nest(pool(temp))
 
-        temp = self.nest(flat(temp['shape']))
-        temp = self.nest(fc(temp['shape'][2], 4096))
-        temp = self.nest(fc(temp['shape'][2], temp['shape'][2]))
-        temp = self.nest(fc(temp['shape'][2], 1000))
+        temp = self.nest(flat(temp))
+        temp = self.nest(fc(temp, 4096))
+        temp = self.nest(fc(temp, temp['output'][2]))
+        temp = self.nest(fc(temp, 1000))
         
 
 if __name__ == '__main__':
 
     # calc
-    # ignore computations of activation functions, pooolings, etc.
-    print('-'*32)
-    print_madd(AlexNet().return_dict())
+    # ignore computations of activation functions, pooolings, concatenate, Non maximum surpression, etc.
+    print('\n'+'-'*32+'\n')
 
-    print('-'*32)
-    print_madd(VGG16().return_dict())
+    print_madd(AlexNet().return_dict(), print_all=False)
+
+    print('\n'+'-'*32+'\n')
+
+    print_madd(VGG16().return_dict(), print_all=False)
+
+    print('\n'+'-'*32+'\n')
+
+    n_class = 6
+    ssd  = VGG16(input=[300, 300, 3], name='Single Shot multibox Detection')
+    fm1  = ssd.del_layers(-9)
+    temp = ssd.nest(conv(fm1, 4*(n_class+4)))
+    o1   = flat(temp)
+    
+    temp = ssd.nest(pool(fm1))
+    temp = ssd.nest(conv(temp, temp['output'][2]*2))
+    fm2  = ssd.nest(p_conv(temp))
+    temp = ssd.nest(conv(fm2, 6*(n_class+4)))
+    o2   = flat(temp)
+
+    temp = ssd.nest(p_conv(fm2, fm2['output'][2]*0.25))
+    fm3  = ssd.nest(conv(temp, temp['output'][2]*2, stride=[2,2]))
+    temp = ssd.nest(conv(fm3, 6*(n_class+4)))
+    o3   = flat(temp)
+
+    temp = ssd.nest(p_conv(fm3, fm3['output'][2]*0.25))
+    fm4  = ssd.nest(conv(temp, temp['output'][2]*2, stride=[2,2]))
+    temp = ssd.nest(conv(fm4, 6*(n_class+4)))
+    o4   = flat(temp)
+
+    temp = ssd.nest(p_conv(fm4, fm4['output'][2]*0.5))
+    fm5  = ssd.nest(conv(temp, temp['output'][2]*2, padding=False))
+    temp = ssd.nest(conv(fm5, 4*(n_class+4)))
+    o5   = flat(temp)
+
+    temp = ssd.nest(p_conv(fm5, fm5['output'][2]*0.5))
+    fm6  = ssd.nest(conv(temp, temp['output'][2]*2, padding=False))
+    temp = ssd.nest(conv(fm6, 4*(n_class+4)))
+    o6   = flat(temp)
+
+    ssd.nest(concat([o1, o2, o3, o4, o5, o6]))
+
+    print_madd(ssd.return_dict(), print_all=True)
+
+    print('\n'+'-'*32+'\n')
+
